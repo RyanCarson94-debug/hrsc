f2b8c4b5a4a8990a8c769d5eb8517d3e2cfb0e46
RyanCarson94-debug <Ryan.Carson94@icloud.com>
2026-03-27 18:18:48 +0000

    add contract builder at /contracts

--- contract-builder/vite.config.js
+++ contract-builder/vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "dist" },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8788",
-        changeOrigin: true,
-      },
-    },
-  },
-});
+  base: '/contracts/',
+  build: {
+    outDir: '../dist/contracts',
+    emptyOutDir: true,
+  }
+})
